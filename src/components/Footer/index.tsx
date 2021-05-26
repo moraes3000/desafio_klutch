4a3a9b5f0431af71c5280843c276e0db799599b2
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-05-26 15:56:20 -0300

    footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+import styles from './footer.module.scss'
+export default function Footer() {
+    return (
+        <footer className={styles.containerfooter}>
+            Nome: Tabela Padrão Parcelas: Valor da Parcela: <button>Avançar</button>
+        </footer>
+    )
+}
--- src/components/TelaHome/index.tsx
+++ src/components/TelaHome/index.tsx
@@ -1,24 +1,27 @@
+import Footer from '../Footer'
 import Tabela from '../Tabela'
 import styles from './telaHome.module.scss'
 
 
 export default function TelaHome() {
   return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.telaHomeHeader}>
+          <img src="Grupo270.svg" alt='plus' className={styles.plus} />
+          <img src="_ionicons_svg_md-filing-2.svg" alt='plus' className={styles.envelopeicon} />
+          <h1>Simulação de Taxas</h1>
+        </div>
+        <div className={styles.telaHomeMain}>
+          <h2>Valor desejado</h2>
+          <input type='number' />
+          <button type='submit' placeholder='R$ 0,00'>Calcular</button>
+        </div>
 
-    <div className={styles.container}>
-      <div className={styles.telaHomeHeader}>
-        <img src="Grupo270.svg" alt='plus' className={styles.plus} />
-        <img src="_ionicons_svg_md-filing-2.svg" alt='plus' className={styles.envelopeicon} />
-        <h1>Simulação de Taxas</h1>
+        <Tabela />
+
       </div>
-      <div className={styles.telaHomeMain}>
-        <h2>Valor desejado</h2>
-        <input type='number' />
-        <button type='submit' placeholder='R$ 0,00'>Calcular</button>
-      </div>
-
-      <Tabela />
-
-    </div>
+      <Footer />
+    </>
   )
 }
