3d93d91dcede1d1453baf6ad8edc7614e72b529f
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-05-26 18:17:47 -0300

    buscacliente

--- src/pages/cliente/index.tsx
+++ src/pages/cliente/index.tsx
@@ -9,15 +9,16 @@ export default function Cliente() {
 
             <div className={styles.container}>
                 <Cabecalho />
-                <div>
+                <div className={styles.search}>
                     <h2>Busque o Cliente</h2>
                     <input type="text" placeholder='000.000.000-00' /><button>Buscar</button>
+
                 </div>
 
-                <div>
-                    <h2>Busque o Cliente</h2>
+                <div className={styles.detalhe}>
+            
                     <h2>Cliente encontrado:</h2>
-                    <h2>000.000.000-00</h2>
+                    <h2 className={styles.cpf}>000.000.000-00</h2>
                     <button>Solicitar</button>
                 </div>
             </div>
