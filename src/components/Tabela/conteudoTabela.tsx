96ff6db7f6d3103255cfc5fa85d384f1316049b1
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-05-26 13:18:45 -0300

    tabela

---
+++ src/components/Tabela/conteudoTabela.tsx
@@ -0,0 +1,17 @@
+import styles from './tabela.module.scss'
+
+
+export default function ConteudoTabela() {
+    return (
+
+     <tr>
+         <td>1</td>
+         <td>1</td>
+         <td>1</td>
+         <td>1</td>
+         <td>1</td>
+
+     </tr>
+
+    )
+}
---
+++ src/components/Tabela/index.tsx
@@ -0,0 +1,34 @@
+import styles from './tabela.module.scss'
+import ConteudoTabela from './conteudoTabela'
+
+export default function Tabela() {
+    return (
+
+        <div className={styles.telaTabela}>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th colSpan={5} >
+                            <h2>Tabela Padrão</h2>
+                        </th>
+                    </tr>
+                    <tr>
+                        <th>Parcela</th>
+                        <th>Juros da Parcela</th>
+                        <th>Valor Parcela</th>
+                        <th>Valor Total</th>
+                        <th>Comissão Parceiro</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <ConteudoTabela />
+                    <ConteudoTabela />
+                    <ConteudoTabela />
+                    <ConteudoTabela />
+                </tbody>
+            </table>
+        </div>
+
+    )
+}
