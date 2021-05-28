567ac6e841f7a04f1709979a76eb1448b5e774d5
Bruno Barbeiro Moraes <bruno_bmoraes@hotmail.com>
2021-05-28 11:27:35 -0300

    check de valor

--- src/components/TelaHome/index.tsx
+++ src/components/TelaHome/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Footer from '../Footer'
 import Tabela from '../Tabela'
 import Cabecalho from './cabecalho'
@@ -5,19 +6,34 @@ import styles from './telaHome.module.scss'
 
 
 export default function TelaHome() {
+  const [valorEmprestimo, setValorEmprestimo] = useState(0)
+  const mensagem1= 'valor abaixo'
+  const mensagem2= 'valor muito acima do limite'
+
+ 
+  function verificaValor(event) {
+    event.preventDefault();
+    console.log({
+      valorEmprestimo,
+    })
+    if (valorEmprestimo <= 300) {      
+      return alert(mensagem1)
+    }
+    if (valorEmprestimo >= 10000) {  
+      return alert(mensagem2)
+    }  
+  }
+
   return (
     <>
       <div className={styles.container}>
-    
+
         <Cabecalho />
-        <div className={styles.telaHomeMain}>
+        <form className={styles.telaHomeMain} onSubmit={verificaValor}>
           <h2>Valor desejado</h2>
-          <input type='number' />
-          <button type='submit' placeholder='R$ 0,00'>Calcular</button>
-        </div>
-
-        {/* <Tabela /> */}
-
+          <input type='number' placeholder='R$ 0,00' value={valorEmprestimo} onChange={event => setValorEmprestimo(Number(event.target.value))} />
+          <button type='submit'>Calcular</button>
+        </form> 
       </div>
       {/* <Footer /> */}
     </>
